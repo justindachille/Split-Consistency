e9c95aa67c6a8395c311189bae928041010358b1
justin-ucdavis <jtdachille@ucdavis.edu>
2024-05-23 14:30:20 -0700

    Updated log graphing file and LaTeX table generation

--- read_global_model_logs.py
+++ read_global_model_logs.py
@@ -3,7 +3,6 @@ from ast import literal_eval
 import argparse
 import numpy as np
 
-
 DEBUG = False
 
 def calc_stats(group):
@@ -25,10 +24,8 @@ pd.set_option('display.width', None)
 # Define an argument parser and add arguments
 parser = argparse.ArgumentParser(description='Process log files.')
 parser.add_argument('--filename', type=str, help='CSV filename')
-parser.add_argument('--alg', type=str, help='Algorithm name')
 parser.add_argument('--dataset_filter', type=str, help='Dataset to filter by')
 parser.add_argument('--partition', type=str, help='Partition to filter by', required=True)
-# parser.add_argument('--alpha', type=float, help='Alpha value to filter by', required=True)
 args = parser.parse_args()
 
 # Load the CSV file into a DataFrame
@@ -40,27 +37,22 @@ def parse_hyperparameters(hyperparams):
 # Parse the 'Hyperparameters' column to a dictionary
 df['Params'] = df['Hyperparameters'].apply(parse_hyperparameters)
 
-# Create a column for 'alpha' by extracting the value from the 'Params' column
-# df['alpha'] = df['Params'].apply(lambda x: x.get('alpha'))
-
-# Create a column for 'split_layer' by extracting the value from the 'Params' column
 df['split_layer'] = df['Params'].apply(lambda x: x.get('split_layer'))
-
-# Create a column for 'partition' by extracting the value from the 'Params' column
 df['partition'] = df['Params'].apply(lambda x: x.get('partition'))
+df['alg'] = df['Params'].apply(lambda x: x['alg'])
+df['n_parties'] = df['Params'].apply(lambda x: x.get('n_parties'))
+df['model'] = df['Params'].apply(lambda x: x['model'])
 
-# Apply filters based on dataset, partition, alpha, and algorithm
+# Apply filters based on dataset and partition
 df = df[(df['Params'].apply(lambda x: x['dataset'] == args.dataset_filter)) &
         (df['partition'] == args.partition) &
-        # (df['alpha'] == args.alpha) &
-        (df['Params'].apply(lambda x: x['alg'] == args.alg)) &
         (df['Client ID'] == 0)]
 
 if DEBUG:
     print("Filtered DataFrame based on specified criteria:")
     print(df.to_string(index=False))
 
-groupby_columns = ['partition', 'split_layer']
+groupby_columns = ['partition', 'split_layer', 'alg', 'n_parties', 'model']
 grouped = df.groupby(groupby_columns)
 
 # Calculate the stats for each group
@@ -68,34 +60,38 @@ stats = grouped.apply(calc_stats).reset_index()
 
 def getTitle(alg, split_layer):
     algorithm = ""
-    if args.alg == "local_training":
+    if alg == "local_training":
         algorithm = "Local Training"
-    elif args.alg == 'sflv1':
+    elif alg == 'sflv1':
         algorithm = f"SFL-V1 ($L_c={split_layer}$)"
-    elif args.alg == 'sflv2':
+    elif alg == 'sflv2':
         algorithm = f"SFL-V2 ($L_c={split_layer}$)"
-    elif args.alg == 'fedavg':
+    elif alg == 'fedavg':
         algorithm = "Fedavg"
-    elif args.alg == 'fedprox':
+    elif alg == 'fedprox':
         algorithm = "Fedprox"
-    elif args.alg == 'moon':
+    elif alg == 'moon':
         algorithm = 'MOON'
+    elif alg == 'centralized':
+        algorithm = 'Centralized'
     else:
-        assert(ValueError("Bad alg input"))
+        raise ValueError("Bad alg input")
     return algorithm
 
 def print_latex_table(stats_df, partition):
     print(f"% Results for partition={partition}")
     print("\\begin{table}[h]")
     print("\\centering")
-    print(f"\\caption{{Training results for data:{partition}, dataset: {args.dataset_filter}}}")
+    # We take the max of n_parties to account for centralized runs, which have n_parties == 1
+    print(f"\\caption{{Training results for data:{partition}, dataset: {args.dataset_filter}, num\\textunderscore users: {max(stats_df['n_parties'])}, model: {stats_df['model'][0]} }}")
+
     print("\\begin{adjustbox}{max width=\\textwidth}")
     print("\\begin{tabular}{|l|c|c|c|} \\hline")
     headers = "Algorithm & Global Test Acc. & Global Top5 Test Acc. & Runs \\\\ \\hline"
     print(headers)
     for _, row in stats_df.iterrows():
-        split_layer = int(row['split_layer'])
-        algorithm = getTitle(args.alg, split_layer)
+        split_layer = int(row['split_layer']) if not pd.isna(row['split_layer']) else ''
+        algorithm = getTitle(row['alg'], split_layer)
         global_test_acc = f"{row['mean_global_accuracy']:.2f} ± {row['std_global_accuracy']:.2f}"
         global_top5_test_acc = f"{row['mean_global_top5_accuracy']:.2f} ± {row['std_global_top5_accuracy']:.2f}"
         runs = int(row['runs'])
@@ -106,5 +102,5 @@ def print_latex_table(stats_df, partition):
 
 # Print LaTeX tables for varying partitions
 for partition in stats['partition'].unique():
-    partition_stats = stats[stats['partition'] == partition]
+    partition_stats = stats[stats['partition'] == partition].sort_values(by='alg')
     print_latex_table(partition_stats, partition)
